3021bd6139d0c2fcf144c66e654bb2f78118d43b
APIVerve Team <hello@apiverve.com>
2024-08-20 11:09:16 -0500

    Bug Fixes

---
+++ StreetAddressParserQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class StreetAddressParserQueryOptions {
+public string address { get; set; }
+}
+
+}
